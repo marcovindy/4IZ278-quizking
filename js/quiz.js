540c16107bea9ee571239ce47b805bc6aead7b36
Marek Vaníček <vanm32@vse.cz>
2022-06-05 19:12:58 +0200

    save

--- js/quiz.js
+++ js/quiz.js
@@ -29,9 +29,9 @@ function showQuestion(question) {
   questionElement.innerText = question.question
   question.answers.forEach(answer => {
     const button = document.createElement('button')
-    button.innerText = answer.text
+    button.innerText = answer.answer
     button.classList.add('btn')
-    if (answer.correct) {
+    if (answer.correct == 1) {
       button.dataset.correct = answer.correct
     }
     button.addEventListener('click', selectAnswer)
@@ -76,37 +76,46 @@ function clearStatusClass(element) {
   element.classList.remove('wrong')
 }
 
-const questions = [
+const questions2 = [
   {
-    question: 'What is 2 + 2?',
-    answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
+    "question": 'What is 2 + 2?',
+    "answers": [
+      { "text": '4', correct: true },
+      { "text": '22', correct: false }
     ]
   },
   {
     question: 'Who is the best YouTuber?',
     answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
+      { "text": 'Web Dev Simplified', correct: true },
+      { "text": 'Traversy Media', correct: true },
+      { "text": 'Dev Ed', correct: true },
+      { "text": 'Fun Fun Function', correct: true }
     ]
   },
   {
     question: 'Is web development fun?',
     answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
+      { "text": 'Kinda', correct: false },
+      { "text": 'YES!!!', correct: true },
+      { "text": 'Um no', correct: false },
+      { "text": 'IDK', correct: false }
     ]
   },
   {
     question: 'What is 4 * 2?',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { "text": '6', correct: false },
+      { "text": '8', correct: true }
     ]
   }
+]
+
+const questions = [
+  {
+    "id":"1",
+    "question":"Kde se používá metoda nejmenších čtverců?",
+    "answers":[
+      {"id":"1","answer":"Při posouzení vhodnosti modelu u regresních funkcí","correct":"0"}
+      ,{"id":"2","answer":"Při určování parametrů regresních funkcí","correct":"1"},{"id":"3","answer":"Při posouzení rozdílnosti rozptylů v ANOVA testu","correct":"0"}]}
 ]
