a5a13e6cd9bb9551def7985ca2da6245b2e81316
Marek Vaníček <vanm32@vse.cz>
2022-06-08 12:26:02 +0200

    fix(Q&A: prázdné odpovědi)

--- php/add-answer.php
+++ php/add-answer.php
@@ -3,23 +3,29 @@
 //načteme připojení k databázi
 require_once '../inc/db.php';
 
-$errors = [];
 if (!empty($_POST)) {
     $quizId = $_POST['quiz_id'];
     $questionId = $_POST['question_id'];
-    $answer = $_POST['answer'];
+    $answer = trim(@$_POST['answer']);
     $answerCorrect = $_POST['answer_correct'];
 
-    if (!empty($answer)){
+    if (!empty($answer)) {
         $query = $db->prepare('INSERT INTO answers (question_id, answer_answer, answer_correct) VALUES (:question_id, :answer, :answer_correct);');
         $query->execute([
             ':question_id' => $questionId,
             ':answer' => $answer,
             ':answer_correct' => $answerCorrect
         ]);
-        $url = "Location: ../custom-edit.php?quiz_id=";
-        $url .= $quizId;
-        header($url);
+        echo "Odpověď úspěšně vytvořena.";
+        header("refresh:2; url=../custom-edit.php?quiz_id=".$quizId);
         exit();
-    }
+    } else {
+        echo "Odpověď nemůže být prázdná!";
+        header("refresh:2; url=../custom-edit.php?quiz_id=".$quizId);
+        exit();
+    } 
+}else {
+    echo "Nepovedlo se vytvořit odpověď.";
+    header("refresh:2; url=../custom-edit.php?quiz_id=".$quizId);
+    exit();
 }
--- php/add-question.php
+++ php/add-question.php
@@ -3,20 +3,27 @@
 //načteme připojení k databázi
 require_once '../inc/db.php';
 
-$errors = [];
-if (!empty($_POST)) {
+if (!empty($_POST['quiz_id'])) {
     $quizId = $_POST['quiz_id'];
-    $question = $_POST['question'];
+    $question = trim(@$_POST['question']);
 
-    if (!empty($question)){
+    if (!empty($question)) {
         $query = $db->prepare('INSERT INTO questions (question_quiz_id, question_question) VALUES (:question_quiz_id, :question_question);');
         $query->execute([
             ':question_quiz_id' => $quizId,
             ':question_question' => $question
         ]);
-        $url = "Location: ../custom-edit.php?quiz_id=";
-        $url .= $quizId;
-        header($url);
+        echo "Otákza úspěšně vytvořena.";
+        header("refresh:2; url=../custom-edit.php?quiz_id=".$quizId);
+        exit();
+    } else {
+        echo "Otákza nemůže být prázdná!";
+        header("refresh:2; url=../custom-edit.php?quiz_id=".$quizId);
         exit();
     }
+   
+} else {
+    echo "Musíte vybrat kvíz.";
+    header("refresh:2; url=../custom-edit.php");
+    exit();
 }
